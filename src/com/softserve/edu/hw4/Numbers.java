1701c256df94d9edf81b7dfac94566c5d479098c
kbuchkovskaya <bu4.k1998@gmail.com>
2022-01-29 16:53:27 +0200

    fixed conditions

--- src/com/softserve/edu/hw4/Numbers.java
+++ src/com/softserve/edu/hw4/Numbers.java
@@ -8,23 +8,30 @@ public class Numbers {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("1: ");
         float a = scanner.nextFloat();
+        System.out.print("2: ");
         float b = scanner.nextFloat();
+        System.out.print("3: ");
         float c = scanner.nextFloat();
 
-        if (a > b && a > c){
-            if (b > c && b < a){
+        System.out.print("Result: ");
+
+        if (a >= b && a >= c){
+            if (b >= c){
                 System.out.println(c + " " + b + " " + a);
-            } else System.out.println(b + " " + c + " " + a);
-        } else if (b > a && b > c){
-            if (a < b && a > c){
+            } else  if (c >= b){
+                System.out.println(b + " " + c + " " + a);
+            }
+        } else if (b >= a && b >= c){
+            if (a >= c){
                 System.out.print(c + " " + a + " " + b);
             } else System.out.print(a + " " + c + " " + b);
-        } else if (c > a && c > b){
-            if (b < c && b > a){
+        } else if (c >= a && c >= b){
+            if (b >= a){
                 System.out.print(a + " " + b + " " + c);
             } else System.out.print(b + " " + a + " " +c);
-        }
+        } else System.out.println(a + " " + b + " " + c);
     }
 
 }
