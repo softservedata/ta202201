c65771f3a0074af6da547dd08717b2ad479be851
VVogur <36006268+VVogur@users.noreply.github.com>
2022-02-01 22:04:15 +0200

    - change int to double - added few sout's

--- src/com/softserve/edu/hw4/HW4.java
+++ src/com/softserve/edu/hw4/HW4.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class HW4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a, b, c;
+        double a, b, c;
 
+        System.out.println("You should set any number in format \"1\" or \"1,1\"");
         System.out.println("Enter first number: ");
-        a = sc.nextInt();
+        a = sc.nextDouble();
         System.out.println("Enter second number: ");
-        b = sc.nextInt();
+        b = sc.nextDouble();
         System.out.println("Enter third number: ");
-        c = sc.nextInt();
+        c = sc.nextDouble();
 
-        int[] array = new int[]{a, b, c};
+        System.out.println("result in ascending order: ");
+        double[] array = new double[]{a, b, c};
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
     }
