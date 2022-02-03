c0f920b760d07920d46a395a796282ac263d04d5
Vladyslav <vladyslav.aleksandrov96@gmail.com>
2022-02-03 22:58:21 +0200

    Added Hw4

--- src/com/softserve/edu/hw5/Vlad_Homework_5.java
+++ src/com/softserve/edu/hw5/Vlad_Homework_5.java
@@ -1,2 +1,8 @@
-package com.softserve.edu.hw5;public class Vlad_Homework_5 {
+package com.softserve.edu.hw5;
+
+public class Vlad_Homework_5 {
+    public static void main(String[] args)
+    {
+        //Nothing here, go away, I'll do it on the weekend
+    }
 }
