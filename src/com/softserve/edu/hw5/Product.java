77c06a8258824bc827e9a04da1e6862dbf8db5db
VVogur <36006268+VVogur@users.noreply.github.com>
2022-02-03 19:03:34 +0200

    - fix

--- src/com/softserve/edu/hw5/Product.java
+++ src/com/softserve/edu/hw5/Product.java
@@ -1,9 +1,6 @@
 package com.softserve.edu.hw5;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 public class Product {
     private String name;
