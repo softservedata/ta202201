ca553a068f0e80e1ecd6896a0dbcb2d58d10d122
Vladyslav <vladyslav.aleksandrov96@gmail.com>
2022-02-03 23:06:29 +0200

    Hw5 is due Sunday...

--- src/com/softserve/edu/hw5/Vlad_Homework_5.java
+++ src/com/softserve/edu/hw5/Vlad_Homework_5.java
@@ -3,6 +3,6 @@ package com.softserve.edu.hw5;
 public class Vlad_Homework_5 {
     public static void main(String[] args)
     {
-        //Nothing here, go away, I'll do it on the weekend
+        //Nothing here, I'll do it on the weekend
     }
 }
