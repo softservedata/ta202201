42cb1eab503258f42a2602e79e1197dccd6fc247
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-02-07 01:13:46 +0200

    opHomework6

---
+++ src/com/softserve/edu/hw6/Developer.java
@@ -0,0 +1,33 @@
+package com.softserve.edu.hw6;
+
+import java.util.Objects;
+
+public class Developer extends Person{
+    private String direction;
+
+    public Developer(String name, int age, String direction) {
+        super(name, age);
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Developer developer = (Developer) o;
+        return direction.equals(developer.direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), direction);
+    }
+}
---
+++ src/com/softserve/edu/hw6/Person.java
@@ -0,0 +1,48 @@
+package com.softserve.edu.hw6;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
