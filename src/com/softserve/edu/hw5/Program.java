0562833b559d4f1f79160bead5dde9043cb0af6f
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-02-07 00:55:08 +0200

    opHomework5

--- src/com/softserve/edu/hw5/Program.java
+++ src/com/softserve/edu/hw5/Program.java
@@ -2,8 +2,12 @@ package com.softserve.edu.hw5;
 
 public class Program {
     public static void main (String[] args) {
-        Product prods[] = {new Product("Apple", 40), new Product("Orange", 40), new Product("Apple", 45), new Product("Orange", 50), new Product("Kiwi", 50)};
+        Product[] prods = {new Product("Apple", 40), new Product("Orange", 40), new Product("Apple", 45), new Product("Orange", 50), new Product("Kiwi", 50)};
 
-
+        for (int i=0; i<prods.length; i++) {
+            for (int j = 0; j < prods[i].length; j++) {
+                System.out.println(prods[i][j].getProductName + " ");
+            }
+        }
     }
 }
