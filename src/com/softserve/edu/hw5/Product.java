ff6f9d990b7892c95d89ca5eb6893269fc2753ea
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-02-07 22:18:36 +0200

    opHomework5

--- src/com/softserve/edu/hw5/Product.java
+++ src/com/softserve/edu/hw5/Product.java
@@ -1,9 +1,15 @@
 package com.softserve.edu.hw5;
 
+import java.util.Objects;
+
 public class Product {
     private String productName;
     private int productPrice;
 
+    public Product () {
+        this.productName = "";
+        this.productPrice = 0;
+    }
     public Product(String productName, int productPrice) {
         this.productName = productName;
         this.productPrice = productPrice;
@@ -12,6 +18,7 @@ public class Product {
     public void setProductName(String productName) {
         this.productName = productName;
     }
+
     public void setProductPrice(int productPrice) {
         this.productPrice = productPrice;
     }
@@ -19,7 +26,48 @@ public class Product {
     public String getProductName() {
         return productName;
     }
+
     public int getProductPrice() {
         return productPrice;
     }
-}
+
+    @Override
+    public String toString() {
+        return "\nProduct " + productName + " at " + productPrice;
+    }
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if ((o == null) || (getClass() != o.getClass())) {
+            return false;
+            Product other = (Product) o;
+            if ((productPrice != other.productPrice)
+                    || ((productName == null) && (other.productName != null))
+                    || ((productName != null) && (other.productName == null))) {
+                return false;
+            }
+            if ((productName == null) && (other.productName == null)) {
+                return true;
+                return other.equals(other.productName);
+                //return productPrice == product.productPrice && Objects.equals(productName, product.productName);
+            }
+        }
+    }
+
+ */
+/*
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, productPrice);
+    }
+ */
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + productPrice;
+            result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+            return result;
+        }
+    }
--- src/com/softserve/edu/hw5/Program.java
+++ src/com/softserve/edu/hw5/Program.java
@@ -2,12 +2,14 @@ package com.softserve.edu.hw5;
 
 public class Program {
     public static void main (String[] args) {
-        Product[] prods = {new Product("Apple", 40), new Product("Orange", 40), new Product("Apple", 45), new Product("Orange", 50), new Product("Kiwi", 50)};
+        Product[] prods = {new Product("Apple", 40), new Product("Orange", 41), new Product("Apple", 45), new Product("Orange", 50), new Product("Kiwi", 50)};
 
-        for (int i=0; i<prods.length; i++) {
-            for (int j = 0; j < prods[i].length; j++) {
-                System.out.println(prods[i][j].getProductName + " ");
+        for (int i=0; i< prods.length; i++) {
+            for (int j=1; j<prods.length; j++) {
+            if (prods[i].equals(prods[j])) {
+                System.out.println(prods[i]);
             }
+        }
         }
     }
 }
