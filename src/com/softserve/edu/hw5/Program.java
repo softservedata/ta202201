642b9907d89bf545bee1c2f8b27db7efd8b48554
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-02-18 08:20:04 +0200

    opHomework6

--- src/com/softserve/edu/hw5/Program.java
+++ src/com/softserve/edu/hw5/Program.java
@@ -9,11 +9,18 @@ public class Program {
                 new Product("Orange", 41),
                 new Product("Apple", 40),
                 new Product("Orange", 41),
+                new Product("Apple", 40),
+                new Product("Orange", 41),
                 new Product("Kiwi", 51)};
+        boolean isFound = false;
 
-        for (int i=0; i< prods.length; i++) {
+        for (int i=0; i< prods.length-1; i++) {
+            if (prods[i] == null) continue;
             for (int j=i+1; j< prods.length; j++) {
-                if (prods[i].equals(prods[j])) System.out.println(prods[j]);
+                if (prods[i].equals(prods[j])) {
+                    if (!isFound) {
+                        System.out.println(prods[i]); }
+                        prods[j] = null; }
             }
             }
     }
--- src/com/softserve/edu/hw6/App6.java
+++ src/com/softserve/edu/hw6/App6.java
@@ -26,7 +26,8 @@ public class App6 {
                 new Developer("Richard", 25, "C#"),
                 new Developer("Valentyn", 30, "Java"),
                 new Developer("Dmytro K", 27, "Java"),
-                new Developer("Oleh", 45, "Java")
+                new Developer("Oleh", 45, "Java"),
+                new Developer("Andrew", 27, "Java")
         };
             /* getting the youngest Java dev
             int imin = 0;
@@ -40,7 +41,7 @@ public class App6 {
             System.out.println(devs[imin]);
             */
             // Sorting out the array and printing two min age devs
-            Developer tmp;
+                 Developer tmp;
             for (int i=0; i<devs.length; i++) {
                 for (int j=i+1; j<devs.length; j++)
                     if (devs[i].getAge()>devs[j].getAge()) {
@@ -53,5 +54,7 @@ public class App6 {
                 if (devs[i].getDirection().equals("Java")) {
                     System.out.println(devs[i]); } // printing out all Java devs
                 }
+
+
     }
 }
