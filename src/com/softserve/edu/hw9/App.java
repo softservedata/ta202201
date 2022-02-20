d475179fa81dba752f59fc179c46f2b0a84a5542
zlazu <zorianaholub@gmail.com>
2022-02-20 18:11:09 +0200

    hw9

---
+++ src/com/softserve/edu/hw9/App.java
@@ -0,0 +1,38 @@
+package com.softserve.edu.hw9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        List<Integer> lstInt1 = new ArrayList<>();
+        lstInt1.add(2);
+        lstInt1.add(4);
+        lstInt1.add(4);
+        lstInt1.add(3);
+        lstInt1.add(1);
+        List<Integer> lstInt2 = new ArrayList<>();
+        lstInt2.add(1);
+        lstInt2.add(2);
+        lstInt2.add(3);
+        lstInt2.add(4);
+        lstInt2.add(5);
+        List<Integer> lstInt3 = new ArrayList<>();
+        lstInt3 = App.commonElem(lstInt1,lstInt2);
+        System.out.println(lstInt3);
+    }
+    private static List<Integer> commonElem(List<Integer> lstInt1, List<Integer> lstInt2){
+        System.out.println("elements: " + lstInt1);
+        System.out.println("elements: " + lstInt2);
+        List<Integer> lstInt3 = new ArrayList<>();
+        for (int i = 0; i < lstInt1.size(); i++) {
+                if (lstInt2.contains(lstInt1.get(i)) && !lstInt3.contains(lstInt1.get(i))){
+                    System.out.println(lstInt1.get(i));
+                    lstInt3.add(lstInt1.get(i));
+            }
+        }
+        System.out.println(lstInt3);
+        return(lstInt3);
+    }
+}
+
