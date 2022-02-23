1661a47c69ec66c94ccee23cdde52cc1c13bc102
irynabevz <bewz1006@gmail.com>
2022-02-23 02:20:02 +0200

    Add two ArrayList. Create method for finding matched elements.

---
+++ src/com/softserve/edu/hw9/App.java
@@ -0,0 +1,31 @@
+package com.softserve.edu.hw9;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args){
+        List<Integer> firstList = new ArrayList<Integer>()
+        {{
+            add(0);
+            add(5);
+            add(51);
+            add(17);
+        }};
+
+        List<Integer> secondlist = new ArrayList<Integer>()
+        {{
+            add(0);
+            add(27);
+            add(53);
+            add(17);
+        }};
+
+        System.out.println("First List: " + firstList);
+        System.out.println("Second List: " + secondlist);
+
+        MatchArray matchedArray = new MatchArray();
+        matchedArray.getMatchArray(firstList, secondlist);
+        
+    }
+
+}
---
+++ src/com/softserve/edu/hw9/MatchArray.java
@@ -0,0 +1,18 @@
+package com.softserve.edu.hw9;
+
+import java.util.*;
+
+public class MatchArray {
+    public void getMatchArray(List<Integer> firstList, List<Integer> secondList){
+        List<Integer> matchArray = new ArrayList<Integer>();
+
+        for (int i = 0; i < firstList.size(); i++){
+            for (int j = 0; j < secondList.size(); j++){
+                if (firstList.get(i).equals(secondList.get(j))) {
+                    matchArray.add(firstList.get(i));
+                }
+            }
+        }
+        System.out.println(matchArray);
+    }
+}
