910904f3b660a4020c6cf4221b9c8a0accbb65d2
irynabevz <bewz1006@gmail.com>
2022-02-23 09:12:11 +0200

    Add two ArrayList. Create method for finding matched elements.

--- src/com/softserve/edu/hw9/App.java
+++ src/com/softserve/edu/hw9/App.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args){
+
         List<Integer> firstList = new ArrayList<Integer>()
         {{
             add(0);
--- src/com/softserve/edu/hw9/MatchArray.java
+++ src/com/softserve/edu/hw9/MatchArray.java
@@ -13,6 +13,7 @@ public class MatchArray {
                 }
             }
         }
+
         System.out.println(matchArray);
     }
 }
