a5e11fa95400d1cebc5ae2a3523901b0c02101dc
serzez <serzez@gmail.com>
2022-03-28 10:30:34 +0300

    HW13

---
+++ src/com/softserve/edu/hw13/App13.java
@@ -0,0 +1,14 @@
+package com.softserve.edu.hw13;
+
+/*Homework 13 (from 18.03.2022)
+        1) Створити список List<String> names; з іменами людей.
+        Посортувати цей список спочатку  по кількості символів у імені, а в межах однакової кількості - по алфавіту.
+        Для сортування використати лямда вираз
+        Collections.sort( (name1,name2)-> ... );
+        Явно клас нащадка компаратора не створювати.
+        Для коду використати пакет com.softserve.homework13
+        Запушати код на github у свою вітку.*/
+
+public class App13 {
+    // I will finish and push code later by end of the day
+}
